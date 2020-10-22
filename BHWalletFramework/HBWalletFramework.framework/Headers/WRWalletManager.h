9cc635aed870f27422c2c5c2536cab128ca1bf5d
GaiShiDaYingXiong <mars_liu_dev@163.com>
2020-10-22 20:32:47 +0800

    修改消费传参字段

--- BHWalletFramework/HBWalletFramework.framework/Headers/WRWalletManager.h
+++ BHWalletFramework/HBWalletFramework.framework/Headers/WRWalletManager.h
@@ -17,14 +17,11 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (void )openWalletMinePageWithUid:(NSString *)userid currentViewController:(UIViewController *)currentViewController;
 /**
- *@Param userid 用户id
- *@Param payType 0:普通钱包支付 1:绑定卡代扣支付
- *@Param transAmount 金额
- *@Param remark 附言/备注
+ *@Param params  字典包含 字段(1.mUserId 用户id  2.payType 0:普通钱包支付 1:绑定卡代扣支付 3. transAmount 金额 4.remark 附言/备注 5.orderNumber 订单编号 6.requestTime 请求时间)
  *@Param currentViewController 当前控制器(非导航控制器)
- *@Param PayResultBlock 返回支付结果code 0-系统忙，请您稍后再试或与银行联系 1-支付成功 2-未开通钱包账户
+ *@Param PayResultBlock 返回支付结果 0-系统忙，请您稍后再试或与银行联系 1-支付成功 2-未开通钱包账户 3-请勿重复支付 4-订单号不可为空
  */
-+ (void )PayWithUid:(NSString *)userid payType:(NSString *)payType transAmount:(NSString *)transAmount remark:(NSString *)remark  currentViewController:(UIViewController *)currentViewController PayResultBlock:(void(^)(NSString *backCode))PayResultBlock;
++ (void )PayWithParams:(NSDictionary *)params  currentViewController:(UIViewController *)currentViewController PayResultBlock:(void(^)(NSString *backCode))PayResultBlock;
 @end
 
 NS_ASSUME_NONNULL_END
