37c15a18996ef65c4351f84d374043cc3516f474
GaiShiDaYingXiong <mars_liu_dev@163.com>
2020-10-23 17:46:22 +0800

    0.0.9

--- BHWalletFramework/HBWalletFramework.framework/Headers/WRWalletManager.h
+++ BHWalletFramework/HBWalletFramework.framework/Headers/WRWalletManager.h
@@ -14,14 +14,16 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *@Param userid 用户id
  *@Param currentViewController 当前控制器(非导航控制器)
+ *@Param baseUrl 项目基地址
  */
-+ (void )openWalletMinePageWithUid:(NSString *)userid currentViewController:(UIViewController *)currentViewController;
++ (void )openWalletMinePageWithUid:(NSString *)userid currentViewController:(UIViewController *)currentViewController baseUrl:(NSString *)baseUrl;
 /**
  *@Param params  字典包含 字段(1.mUserId 用户id  2.payType 0:普通钱包支付 1:绑定卡代扣支付 3. transAmount 金额 4.remark 附言/备注 5.orderNumber 订单编号 6.requestTime 请求时间)
  *@Param currentViewController 当前控制器(非导航控制器)
  *@Param PayResultBlock 返回数据包含code、msg、data
+ *@Param baseUrl 项目基地址
  */
-+ (void )PayWithParams:(NSDictionary *)params  currentViewController:(UIViewController *)currentViewController PayResultBlock:(void(^)(NSDictionary *responseData))PayResultBlock;
++ (void )PayWithParams:(NSDictionary *)params  currentViewController:(UIViewController *)currentViewController PayResultBlock:(void(^)(NSDictionary *responseData))PayResultBlock baseUrl:(NSString *)baseUrl;
 @end
 
 NS_ASSUME_NONNULL_END
